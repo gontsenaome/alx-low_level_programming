3b65188a6f919b2b763e6227ff9b8fed1477b7ed
gontsenaome <gontsenaome243@gmail.com>
2022-12-13 12:35:41 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,29 +16,32 @@ void jack_bauer(void)
 	h2 = 0;
 	while (h2 <= 2)
 	{
-		/*Restrain  to 23h, not 29*/
-		a = 3;
-	}
-	h1 = 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		if (h2 == 2)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
+			/*Restrain to 23h, not 29*/
+			a = 3;
 		}
-		h1++;
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m1 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
 	}
-	h2++;
 }
