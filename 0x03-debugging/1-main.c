79292d14fb54ca540bba72dbf158f47ed51db946
gontsenaome <gontsenaome243@gmail.com>
2022-12-17 00:04:12 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,18 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/*
-	 * while (i < 10)
-	 * {
-	 *              putchar(i);
-	 * }
-	 */
-	printf("infinite loop avoided! \\o/");
+
+	/**
+	*while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
+
+	printf("Infinity loop avoided! \\o/\n");
 
 	return (0);
 }
