d538ab171681f2b16d22c3b49683683f42743ae0
gontsenaome <gontsenaome243@gmail.com>
2022-12-22 08:30:46 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper - changes all lowercases letters of a string to uppercase
+ *@str: parameter
+ *Return: returns in character
+ */
+
+char *string_toupper(char *)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+
+	return (str);
+}
