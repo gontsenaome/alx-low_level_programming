ad36965934d361545fb12ebf294ebf0c0c45e642
gontsenaome <gontsenaome243@gmail.com>
2023-01-05 04:27:22 -0800

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of string.
+ * _strlen_recursion - Returns the length of a  string.
  * @s: The string to be measured.
  *
  * Return: the length of the string.
@@ -16,3 +16,4 @@ int _strlen_recursion(char *s)
 		len += _strlen_recursion(s + 1);
 	
 	}
+}
