cf2b9f19c50b9943e2f50e8c8b3bfdc9067bf845
gontsenaome <gontsenaome243@gmail.com>
2023-01-10 11:34:11 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,50 @@
-#include "main.h"
 #include <stdlib.h>
+/**
+ * get_str_len - gets the length of the string
+ * @str: string whose length is to be found.
+ * Return: returns integer value of the string.
+ */
+int get_str_len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i + 1);
+}
 
 /**
- * str_concat - concatenates two strings
- * @s1: string to concatenates
- * @s2: other string to concatenates
- *
- * Return: pointer to new string created (success) or NULL (Error)
+ * str_concat - joins two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: contents of s1, followed by the contents of s2, null terminated.
+ * if NULL is passed, treat it as an empty string
+ * The function should return NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int s1_size, s2_size, i, j;
+	char *ptr;
 
-	while (s1 && s1[len1])
-		len1++;
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 
-	while (s2 && s2[len2])
-		len2++;
+	s1_size = get_str_len(s1);
+	s2_size = get_str_len(s2);
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	ptr = (char *) malloc(((s1_size +  s2_size) - 1) * sizeof(char));
+
+	if (ptr == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; s2[j] !=  '\0'; j++, i++)
+		ptr[i] = s2[j];
+	ptr[i] = '\0';
 
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
-
-	if (s2)
-	{
-		while (i < (le1 + len2))
-		{
-			s3[i] = s2[i];
-			i++;
-			j++;
-		}
-	}
-	s3[i] '\0';
-
-	return (s3);
+	return (ptr);
 }
