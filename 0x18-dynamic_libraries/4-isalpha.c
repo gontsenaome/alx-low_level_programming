b9010558675d31882eb90aa2311d507674bc2a68
gontsenaome <gontsenaome243@gmail.com>
2023-03-20 01:55:45 -0700

    All tasks

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for char alphabet
+ * @c: the char to be checked
+ *
+ * Return: 1 if char is letter lower or upper, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
